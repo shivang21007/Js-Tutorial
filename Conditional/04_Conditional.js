b7644ed5551e7af8d30100fc2a56c068a31d6ef6
Shivang21007 <shivanggupta2611@gmail.com>
2022-12-27 01:44:14 +0530

    Conditioanl statements of Jaavascript added by Shivang

---
+++ Conditional/04_Conditional.js
@@ -0,0 +1,15 @@
+let temperature;
+
+
+temperature = 40;
+
+if(temperature < 20){
+    console.log("Its very cold out side");
+
+}
+
+if(temperature < 30){
+    console.log("its moderate temp. outside");
+}else{
+    console.log("Its really really Hot outside");
+}
