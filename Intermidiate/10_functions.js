15ffd1f98a35664c6c1776cccfca578573afcef5
Shivang21007 <shivanggupta2611@gmail.com>
2022-12-30 14:07:13 +0530

    readme updated and Intermidiate concepts added

---
+++ Intermidiate/10_functions.js
@@ -0,0 +1,16 @@
+function sayHello(name) {
+    console.log("Hietsh Chaoudary is Here");
+    console.log(`Hello there : ${name}. How are you?`);
+} 
+
+// sayHello("Hitesh");
+// sayHello("shivang");
+
+
+function Namstey(){
+    return "hello in India";
+}
+
+var greeting = Namstey();
+console.log(greeting);
+console.log(Namstey());
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Js-Tutorial
 This is Tutorial of JS by me .
+
+
+Basic -> Conditional -> Intermidiate -> Not Avance -> DOM -> Advanceish
