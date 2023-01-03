a00d6e57be2e4efd518ef3adecc8034f82c0ed84
Shivang21007 <shivanggupta2611@gmail.com>
2023-01-03 17:31:33 +0530

    new concepts added

---
+++ Intermidiate/18_fillAndFilter.js
@@ -0,0 +1,20 @@
+/*var testArray = [2,4,6,9,7,5,2,3];
+console.log(testArray.fill("h",2,5));
+*/ // fill part
+
+
+
+/*const myNumber = [45,58,2,62,56,98,26,75,22]
+const result = myNumber.filter((num) => num > 55);
+console.log(result);
+*/ //filter part
+
+
+
+// Slice and Splice part
+var user = ["Ted", "Tim","ton","sam", "sor","sod"];
+// console.log(user.slice(2)); // pahle ke saare hata deta h
+
+user.splice(1,3,"Hi","Bye"); // iss range me saare change kr deta h
+user.splice();
+console.log(user);
---
+++ Intermidiate/19_Objects.js
@@ -0,0 +1,13 @@
+ var user = {
+    firstName : "Shivang",
+    lastname : "Gupta",
+    roel : "Admin",
+    loginCount : 54,
+    googleSignIn : true,
+
+ };
+
+ console.log(user);
+//  console.log(user.firstName);
+
+ console.table(user);
