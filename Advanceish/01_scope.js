cbd58ea1034b84f241de2d1499d8ac6a338af98a
Shivang21007 <shivanggupta2611@gmail.com>
2023-01-13 20:22:29 +0530

    Advance concepts added

---
+++ Advanceish/01_scope.js
@@ -0,0 +1,15 @@
+// console.log(name);
+// var name = "hitesh";
+
+if(true){
+    var lastName = "Gupta";
+}
+console.log(lastName);
+// in this as we difiened the variable with var so it still avaible out of his scope which is his func.
+
+
+if(true){
+    let LastName = "Gupta";
+}
+console.log(LastName);
+//in this example we defined the variable with let so it is not available outside his scope i.e. function.
---
+++ Advanceish/02_strings.js
@@ -0,0 +1,7 @@
+ var name = `hitesh`;
+ var fullName = `${name} Chaudhary`;
+ console.log(fullName);
+ console.log(fullName.charAt(0));
+ console.log(fullName.endsWith("y"));
+ console.log(fullName.includes("hitesh"));
+ console.log(fullName.toUpperCase());
